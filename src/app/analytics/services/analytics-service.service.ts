ce0fe99ebbdd4f3c38081342f6bcfcce57be3c6d
paolomv02 <u202010039@upc.edu.pe>
2025-05-16 01:17:06 -0500

    feat(add-reports-table): add incidents model

--- src/app/analytics/services/analytics-service.service.ts
+++ src/app/analytics/services/analytics-service.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { DriverAnalytics } from '../models/driver-analytics.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AnalyticsServiceService {
+  private apiUrl = 'https://jdu202012207.github.io/pruebas-api/analytics-drivers.json';
 
   constructor(private http: HttpClient) { }
 
-  getIncidents(): Observable<any> {
-    return this.http.get('/incidents');
+  getDriverAnalytics(): Observable<DriverAnalytics[]> {
+    return this.http.get<DriverAnalytics[]>(this.apiUrl);
   }
 }
